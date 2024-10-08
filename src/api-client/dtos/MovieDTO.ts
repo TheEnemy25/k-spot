5a712cf57ba8b7315a905b2db821d5df89eb7df4
TheEnemy25 <andrikkobrin@gmail.com>
2024-10-08 09:35:59 +0300

    Added create page for movie

---
+++ src/api-client/dtos/MovieDTO.ts
@@ -0,0 +1,10 @@
+export type MovieDTO = {
+  title: string;
+  ageRestriction: number;
+  description: string;
+  imageLink: string;
+  trailerLink: string;
+  rating: number;
+  duration: string;
+  releaseDate: Date;
+};
--- src/app/admin/movie/create/page.tsx
+++ src/app/admin/movie/create/page.tsx
@@ -1,52 +1,77 @@
-"use client";
+'use client';
 
-import React, { useState } from 'react';
-import Movie from '../../../../api-client/models/movie-models/Movie';
+import { useState } from 'react';
+import { z } from 'zod';
+import './MovieFormPage.module.scss';
 import MovieService from '../../../../api-client/service/MovieService';
-import { AxiosError } from 'axios';
-import "./MovieFormPage.module.scss"
+import { MovieDTO } from '../../../../api-client/dtos/MovieDTO';
 
-interface MovieFormProps {
-    onMovieCreated: (newMovie: Movie) => void;
-}
+const movieSchema = z.object({
+    title: z.string().min(1, { message: "Title is required" }),
+    ageRestriction: z.number().min(0, { message: "Age restriction must be a positive number" }),
+    description: z.string().min(10, { message: "Description must be at least 10 characters" }),
+    imageLink: z.string().url({ message: "Invalid URL format for image link" }).optional(),
+    trailerLink: z.string().url({ message: "Invalid URL format for trailer link" }).optional(),
+    rating: z.number().min(0.1, { message: "Rating must be greater than 0 and less than or equal to 10" }).max(10, { message: "Rating must be between 0 and 10" }),
+    duration: z.string().regex(/^(?!00:00:00$)\d{2}:\d{2}:\d{2}$/, { message: "Duration must be in HH:MM:SS format and cannot be 00:00:00" }),
+    releaseDate: z.date().min(new Date('1900-01-01'), { message: "Invalid release date" }),
+});
 
-const useMovieForm = () => {
-    const INITIAL_FORM_STATE = {
+const CreateMoviePage = () => {
+    const [formData, setFormData] = useState<MovieDTO>({
         title: '',
         ageRestriction: 0,
         description: '',
         imageLink: '',
         trailerLink: '',
         rating: 0,
-        duration: '',
-        releaseDate: ''
-    };
+        duration: '00:00:00',
+        releaseDate: new Date(),
+    });
 
-    const [formData, setFormData] = useState(INITIAL_FORM_STATE);
+    const [errors, setErrors] = useState<{ [key: string]: string }>({});
+
+    const parseValue = (name: string, value: string) => {
+        switch (name) {
+            case 'releaseDate':
+                return new Date(value);
+            case 'ageRestriction':
+            case 'rating':
+                return Number(value);
+            default:
+                return value;
+        }
+    };
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { name, value } = e.target;
-        setFormData(prevState => ({
+
+        setFormData((prevState) => ({
             ...prevState,
-            [name]: value
+            [name]: parseValue(name, value),
         }));
+
+        setErrors((prevErrors) => ({ ...prevErrors, [name]: '' }));
     };
 
-    const resetForm = () => {
-        setFormData(INITIAL_FORM_STATE);
-    };
 
-    return { formData, handleChange, resetForm };
-};
-
-function MovieFormPage({ onMovieCreated }: MovieFormProps) {
-    const { formData, handleChange, resetForm } = useMovieForm();
-
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
+
+        const result = movieSchema.safeParse(formData);
+        if (!result.success) {
+            const validationErrors = Object.fromEntries(
+                Object.entries(result.error.flatten().fieldErrors).map(([key, value]) => [
+                    key,
+                    value[0],
+                ])
+            );
+            setErrors(validationErrors);
+            return;
+        }
+
         try {
-            const releaseDate = new Date(formData.releaseDate);
-            const newMovieData = {
+            await MovieService.createMovie({
                 ...formData,
                 movieActors: [],
                 movieDirectors: [],
@@ -58,59 +83,133 @@ function MovieFormPage({ onMovieCreated }: MovieFormProps) {
                 rentals: [],
                 reviews: [],
                 sessions: [],
-                releaseDate: releaseDate,
-            };
-            const newMovie = await MovieService.createMovie(newMovieData);
-            onMovieCreated(newMovie);
-            resetForm();
+            });
+            alert('Movie created successfully!');
         } catch (error) {
-            if ((error as AxiosError).response) {
-                console.error('Error creating movie:', (error as AxiosError).response?.data);
-            } else {
-                console.error('Unknown error:', error);
-            }
+            console.error('Error creating movie:', error);
         }
     };
-    return (
-        <div className='movie-form-page'>
-            <h2>Create Movie</h2>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Title:
-                    <input type="text" name="title" placeholder="title" value={formData.title} onChange={handleChange} required />
-                </label>
-                <label>
-                    Age Restriction:
-                    <input type="number" name="ageRestriction" placeholder="ageRestriction" value={formData.ageRestriction} onChange={handleChange} required />
-                </label>
-                <label>
-                    Description:
-                    <textarea name="description" placeholder="description" value={formData.description} onChange={handleChange} required />
-                </label>
-                <label>
-                    Image URL:
-                    <input type="text" name="imageLink" placeholder="imageLink" value={formData.imageLink} onChange={handleChange} required />
-                </label>
-                <label>
-                    Trailer Link:
-                    <input type="text" name="trailerLink" placeholder="trailerLink" value={formData.trailerLink} onChange={handleChange} required />
-                </label>
-                <label>
-                    Rating:
-                    <input type="number" name="rating" placeholder="rating" value={formData.rating} onChange={handleChange} required />
-                </label>
-                <label>
-                    Duration:
-                    <input type="text" name="duration" placeholder="duration" value={formData.duration} onChange={handleChange} required />
-                </label>
-                <label>
-                    Release Date:
-                    <input type="date" name="releaseDate" placeholder="releaseDate" value={formData.releaseDate} onChange={handleChange} required />
-                </label>
-                <button type="submit">Create</button>
-            </form>
-        </div>
-    );
-}
 
-export default MovieFormPage;
+    return (
+        <>
+            <section className="create-movie">
+
+                <h1 className="create-movie__title">Create a New Movie</h1>
+
+                <form className="create-movie__form" onSubmit={handleSubmit}>
+                    <div className="create-movie__field">
+                        <label className="create-movie__label">Title</label>
+                        <input
+                            type="text"
+                            className="create-movie__input"
+                            name="title"
+                            value={formData.title}
+                            onChange={handleChange}
+                            placeholder="Enter a title"
+                        />
+                        {errors.title && <span className="error">{errors.title}</span>}
+                    </div>
+
+                    <div className="create-movie__field">
+                        <label className="create-movie__label">Age Restriction</label>
+                        <input
+                            type="number"
+                            className="create-movie__input"
+                            name="ageRestriction"
+                            value={formData.ageRestriction}
+                            onChange={handleChange}
+                            placeholder="Enter an age restriction"
+                        />
+                        {errors.ageRestriction && <span className="error">{errors.ageRestriction}</span>}
+                    </div>
+
+                    <div className="create-movie__field">
+                        <label className="create-movie__label">Description</label>
+                        <textarea
+                            className="create-movie__textarea"
+                            name="description"
+                            value={formData.description}
+                            onChange={handleChange}
+                            placeholder="Enter a description"
+                        ></textarea>
+                        {errors.description && <span className="error">{errors.description}</span>}
+                    </div>
+
+                    <div className="create-movie__field">
+                        <label className="create-movie__label">Image Link</label>
+                        <input
+                            type="text"
+                            className="create-movie__input"
+                            name="imageLink"
+                            value={formData.imageLink}
+                            onChange={handleChange}
+                            placeholder="Enter an image link"
+                        />
+                        {errors.imageLink && <span className="error">{errors.imageLink}</span>}
+                    </div>
+
+                    <div className="create-movie__field">
+                        <label className="create-movie__label">Trailer Link</label>
+                        <input
+                            type="text"
+                            className="create-movie__input"
+                            name="trailerLink"
+                            value={formData.trailerLink}
+                            onChange={handleChange}
+                            placeholder="Enter a trailer link"
+                        />
+                        {errors.trailerLink && <span className="error">{errors.trailerLink}</span>}
+                    </div>
+
+                    <div className="create-movie__field">
+                        <label className="create-movie__label">Rating</label>
+                        <input
+                            type="number"
+                            className="create-movie__input"
+                            name="rating"
+                            step="0.1"
+                            value={formData.rating}
+                            onChange={handleChange}
+                            placeholder="Enter a rating"
+                        />
+                        {errors.rating && <span className="error">{errors.rating}</span>}
+                    </div>
+
+                    <div className="create-movie__field">
+                        <label className="create-movie__label">Duration</label>
+                        <input
+                            type="text"
+                            className="create-movie__input"
+                            name="duration"
+                            value={formData.duration}
+                            onChange={handleChange}
+                            placeholder="Enter a duration in HH:MM:SS"
+                        />
+                        {errors.duration && <span className="error">{errors.duration}</span>}
+                    </div>
+
+                    <div className="create-movie__field">
+                        <label className="create-movie__label">Release Date</label>
+                        <input
+                            type="date"
+                            className="create-movie__input"
+                            name="releaseDate"
+                            value={formData.releaseDate.toISOString().split('T')[0]}
+                            onChange={handleChange}
+                            placeholder="Enter a release date"
+                        />
+                        {errors.releaseDate && <span className="error">{errors.releaseDate}</span>}
+                    </div>
+
+                    <button className="create-movie__button" type="submit">
+                        Create Movie
+                    </button>
+
+                </form>
+
+            </section>
+        </>
+    );
+};
+
+export default CreateMoviePage;
